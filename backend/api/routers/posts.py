6e32b619a83268928e1da82bd35a460ce2814436
Karl-Knapp <xxtheshadowpantherxx@gmail.com>
2025-07-02 18:34:59 -0500

    Update posts.py

--- backend/api/routers/posts.py
+++ backend/api/routers/posts.py
@@ -296,8 +296,9 @@ async def delete_post(post_id: str, user: dict = Depends(login_manager)):
         post = response['Item']
 
         # Allow admins to delete any post, but regular users can only delete their own posts
+        logger.info(f"User attempting delete: {user['username']} with role: {user.get('role')}")
         if post["author"] != user["username"] and user.get("role") != "admin":
-            raise HTTPException(status_code=403, detail=f"Access forbidden: You are not the author of this post. {user.get("role")}")
+            raise HTTPException(status_code=403, detail=f"Access forbidden: You are not the author of this post.")
 
         if "images" in post:
             for image_url in post["images"]:
