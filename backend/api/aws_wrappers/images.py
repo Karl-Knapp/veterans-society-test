2b598531e18dfe0876abf4ef5f1e05b49b790490
Karl-Knapp <xxtheshadowpantherxx@gmail.com>
2025-07-04 16:25:11 -0500

    Update images.py

--- backend/api/aws_wrappers/images.py
+++ backend/api/aws_wrappers/images.py
@@ -1,19 +1,20 @@
 from fastapi import File, UploadFile, HTTPException
+from dotenv import load_dotenv
 import os
 import boto3
 import uuid
 from api.config import S3_BUCKET_NAME
-import traceback
 
-AWS_ACCESS_KEY_ID = os.getenv('aws_access_key_id')
-AWS_SECRET_ACCESS_KEY = os.getenv('aws_secret_access_key')
-AWS_REGION = os.getenv('aws_region')
+load_dotenv()
+# AWS_ACCESS_KEY_ID = os.getenv('aws_access_key_id')
+# AWS_SECRET_ACCESS_KEY = os.getenv('aws_secret_access_key')
+# AWS_REGION = os.getenv('aws_region')
 
 s3_client = boto3.client(
-    's3',
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-    region_name=AWS_REGION
+    's3'
+    # aws_access_key_id=AWS_ACCESS_KEY_ID,
+    # aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    # region_name=AWS_REGION
 )
 
 def upload_file_to_s3(file: UploadFile, file_name: str) -> str:
@@ -22,12 +23,9 @@ def upload_file_to_s3(file: UploadFile, file_name: str) -> str:
     Replace this with your actual S3 upload logic.
     """
     try:
-        file.file.seek(0)
         s3_client.upload_fileobj(file.file, S3_BUCKET_NAME, file_name)
         return f"https://{S3_BUCKET_NAME}.s3.amazonaws.com/{file_name}"
     except Exception as e:
-        print("=== Upload Error Traceback ===")
-        print(traceback.format_exc())  # This gives the full cause
         raise HTTPException(status_code=500, detail=f"Failed to upload image: {str(e)}")
 
 def delete_file_from_s3(file_name: str):
